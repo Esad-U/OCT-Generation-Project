d34110643fff304c0df4965f74f11a164e9fbbab
Esad-U <aesadugur@gmail.com>
2025-05-21 23:02:08 +0300

    Efficient dataset fixed

--- data.py
+++ data.py
@@ -113,6 +113,7 @@ class EfficientDataset(Dataset):
         self.image_size = image_size
         self.window_size = window_size
         self.transform = transform
+        self.samples = []
 
         # Gather all image files
         all_files = [
@@ -120,21 +121,23 @@ class EfficientDataset(Dataset):
             if f.lower().endswith(('.png', '.jpg', '.jpeg')) and '_' in f
         ]
 
-        # Group files by their folder prefix (e.g., "1" from "1_01.png")
+        # Group files by prefix (folder id like "1" from "1_01.png")
         group_dict = {}
         for f in all_files:
-            prefix = f.split('_')[0]
-            if prefix not in group_dict:
-                group_dict[prefix] = []
-            group_dict[prefix].append(f)
+            group_id = f.split('_')[0]
+            if group_id not in group_dict:
+                group_dict[group_id] = []
+            group_dict[group_id].append(f)
 
-        # Sort files within each group and prepare samples
-        self.samples = []
+        # For each group, sort and collect sliding windows of size 3
         for group_id in sorted(group_dict.keys(), key=lambda x: int(x)):
-            files = sorted(group_dict[group_id])
-            if len(files) >= window_size:
-                window = files[:window_size]
-                full_paths = [os.path.join(image_dir, fname) for fname in window]
+            files = sorted(group_dict[group_id])  # sort within the group
+            if len(files) < window_size:
+                continue  # skip short groups
+
+            for i in range(len(files) - window_size + 1):
+                window = files[i:i + window_size]
+                full_paths = [os.path.join(image_dir, f) for f in window]
                 self.samples.append(full_paths)
 
     def __len__(self):
@@ -142,10 +145,11 @@ class EfficientDataset(Dataset):
 
     def __getitem__(self, idx):
         image_paths = self.samples[idx]
+        print(image_paths)
         sequence = []
 
-        for image_file in image_paths:
-            image = Image.open(image_file).convert('L')
+        for image_path in image_paths:
+            image = Image.open(image_path).convert('L')
             image = image.resize((self.image_size, self.image_size))
             image = np.array(image) / 255.0
             sequence.append(image)
